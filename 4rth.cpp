4924b6d9ea4b302bc545ee6edccbd85d45fba8b0
ravinder <ravibeniwal931@gmail.com>
2025-04-20 12:54:41 +0530

    spelling changes

--- 4rth.cpp
+++ 4rth.cpp
@@ -5,11 +5,11 @@ void solve(){
     int n;
     cin >> n;
     if((n+1)%3 == 0 || (n-1)%3 == 0){
-        cout << "FIRST" << endl;
+        cout << "First" << endl;
         return;
     }
     else{
-        cout << "SECOND" << endl;
+        cout << "Second" << endl;
         return;
     }
 }
@@ -21,6 +21,6 @@ int main() {
     while(t--){
         solve();
     }
-    
+
     return 0;
 }
