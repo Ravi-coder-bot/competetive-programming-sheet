4b24e6a113202ce87da3665fba860648b8c50e2a
ravinder <ravibeniwal931@gmail.com>
2025-04-20 12:51:03 +0530

    3rd question of 800 rating problem

---
+++ 3rd.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    if((n+1)%3 == 0 || (n-1)%3 == 0){
+        cout << "FIRST" << endl;
+        return;
+    }
+    else{
+        cout << "SECOND" << endl;
+        return;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
