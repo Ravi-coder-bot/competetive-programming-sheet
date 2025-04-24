c320b7790a45e8688f97cd8557972f60d6517d16
ravinder <ravibeniwal931@gmail.com>
2025-04-24 22:16:40 +0530

    15th

--- 15th.cpp
+++ 15th.cpp
@@ -2,7 +2,53 @@
 using namespace std;
 
 void solve(){
-    
+    int n;
+    cin>> n;
+    vector<int> arr(n);
+
+    for(int i=0;i<n ;i++){
+        cin >> arr[i];
+    }
+
+    int sum1 = arr[0];
+    int sum2 = arr[1];
+
+
+    for(int i=2; i<n ;i++){
+        if(sum1%2==0 && sum2%2==0){
+                sum1 += arr[i];
+
+        }
+        else if(sum1%2==1 && sum2%2==0){
+            if(arr[i]%2==0){
+                sum2 += arr[i];
+            }
+            else{
+                sum1 += arr[i];
+            }
+        }
+        else if(sum1%2==0 && sum2%2==1){
+            if(arr[i]==0){
+                sum1 += arr[i];
+            }
+            else{
+                sum2 += arr[i];
+            }
+
+        }
+        else{
+            sum2 += arr[i];
+        }
+    }
+
+    if(sum1%2==sum2%2){
+        cout << "YES"<< endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+
+
 }
 
 int main() {
