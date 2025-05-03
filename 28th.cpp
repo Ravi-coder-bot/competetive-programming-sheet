97e312eb3885086bbbd4bcbfe32e86a50c298620
ravinder <ravibeniwal931@gmail.com>
2025-05-03 23:29:55 +0530

    updated 28th

--- 28th.cpp
+++ 28th.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool comp(){
-
-
+bool checker(vector<int> arr){
+    int sum = arr[0];
+    for(int i =1 ; i < arr.size(); i++){
+        if(sum == arr[i]){
+            return false;
+        }
+        sum += arr[i];
+    }
+    return true;
 }
 
 void solve(){
@@ -13,17 +19,40 @@ void solve(){
     for(int i=0; i< n; i++){
         cin >> arr[i];
     }
-
-    int sum = 0;
-
-    for(int i=1; i <= n; i++){
-        sum += arr[i-1];
-
-        if(sum==arr[i]){
-            sort(arr.begin(),arr.end(),comp);
-        }
+    if(checker(arr)){
+        cout << "YES" << endl;
+        for(auto i : arr){
+            cout << i << " ";  
+          }
+        cout << endl;
+        return;
     }
 
+    vector<int> temp = arr;
+    sort(temp.begin(), temp.end() );
+
+    if(temp[0] == temp.back()){
+        cout << "NO" << endl;
+        return;
+    }
+
+    vector<int> temp2 = arr;
+    arr[0] = temp2[n-1];
+    for(int i=1 ; i < n ; i++){
+        arr[i] = temp2[i-1]; 
+    }
+
+
+    if(checker(arr)){
+        cout << "YES" << endl;
+        for(auto i : arr){
+            cout << i << " ";  
+          }
+        cout << endl;
+        return;
+    }
+
+    cout << "NO" << endl; 
 }
 
 int main() {
