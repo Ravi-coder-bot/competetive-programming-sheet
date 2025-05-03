caf823025b49a82db77c6a7fdfd7360203e04187
ravinder <ravibeniwal931@gmail.com>
2025-05-03 23:49:49 +0530

    27th updated

--- 27th.cpp
+++ 27th.cpp
@@ -3,55 +3,42 @@ using namespace std;
 
 void solve(){
     int n;
-    cin >> n;
+    cin  >> n ;
+
     vector<int> arr(n);
-    for(int i=0 ;i < n ; i++){
-        cin >>  arr[i];
+    for(int i =0 ; i < n; i++){
+        cin >> arr[i];
     }
 
-    int  i=0;
-    int j =n-1;
-    int mul1 =arr[0];
-    int mul2 =arr[n-1] ;
-    while(j>i){
-        if(mul1> mul2){
-            j--;
-            if(i<j){
-                 mul2 *= arr[j];
-            }
-        }
-        else if(mul2>mul1){
-            i++;
-            if(i<j){
-                 mul1 *= arr[i];
-            }
-           
-        }
-        else if(mul1==mul2){
-            i++;
-            j--;
-            if(i<j){
-            mul1 *= arr[i];
-            mul2 *= arr[j];
-            }
-           
+    int count = 0;
+    for(int i =0 ; i < n ; i++){
+        if(arr[i]==2){
+            count ++;
         }
     }
-
-    if(mul1 == mul2 && i<j){
-        cout << i << endl;
+    if(count == 0){
+        cout << 1 << endl;
         return;
-     }
-    else if(mul1 == mul2 && i>j){
-        cout << i-1 << endl;
-        return;
-     }
-     
-     
-     cout << -1 << endl;
     }
 
-    
+    if(count%2==0){
+        int left = count/2;
+        int  k =0;
+       while(left>0){
+        if(arr[k]==2){
+            left--; 
+        }
+        k++;  
+       }
+       cout << k << endl;
+       return ;
+    }
+    else{
+        cout << -1 << endl;
+        return ;
+    }
+
+}
 
 int main() {
     int t;
