fcd8e21cba8ae7793ecd9862fbad361c9c27952c
ravinder <ravibeniwal931@gmail.com>
2025-05-08 11:01:34 +0530

    2nd 900 rating

--- 900/2nd.cpp
+++ 900/2nd.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 void solve(){
-    string s ;
-    cin >> s;
-
     int n , k ;
     cin >> n >> k ;
+
+    string s;
+    cin >> s;
     map<char, int> freqMap;
 
     for(auto i : s){
         freqMap[i]++;
     }
 
-    char i = s[0] ;
-    while(k--){
-        if(freqMap[i]%2==1){
-            freqMap[i]--;
-        }
-        else{
-            i++;
-        }
-
-        while
-
+    int odds = 0 ;
+    for(auto &c : freqMap){
+        if(c.second%2==1){
+            odds ++;
+        }   
     }
 
-
-
+    int remLen = n - k;
+    int maxAllowedOdd = remLen % 2; 
+        if(k>=odds-maxAllowedOdd){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;  
+        }
 }
 
 int main() {
