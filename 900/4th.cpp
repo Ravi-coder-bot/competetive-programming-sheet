41f18235e61cfb9e57bb27bae2f7f34b015c087d
ravinder <ravibeniwal931@gmail.com>
2025-05-09 23:40:15 +0530

    4th ccp done 😎😎😎

--- 900/4th.cpp
+++ 900/4th.cpp
@@ -1,7 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solve(){
+    int a , b , n;
+    cin >> a >> b >> n ;
+    vector<int> arr(n) ;
+    for(int i =0 ; i< n; i++){
+        cin >> arr[i] ;
+    }
+
+    for(int i =0 ; i < n ; i++){
+        if(arr[i]>=a){
+            arr[i] = a-1;
+        }
+    }
+
+    int sum = 0 ;
+    for(int i = 0 ; i < n ; i++){
+        sum += arr[i] ;
+    }
+
+    sum +=  b;
+
+    cout << sum << endl;
+}
+
 int main() {
+    int t;
+    cin >> t ;
+    while(t--){
+        solve();
+    }
 
     
     return 0;
