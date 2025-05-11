e598f1f29286cd99a5eb51b8b213f0116d1dfaec
ravinder <ravibeniwal931@gmail.com>
2025-05-11 16:15:41 +0530

    6th 900 rating

--- 900/6th.cpp
+++ 900/6th.cpp
@@ -4,34 +4,11 @@ using namespace std;
 void solve(){
     long long n ;
     cin >> n;
-
-    long long  maxCount = 1;
-    
-    for(long long i =1 ; i*i <= n ; i++){
-        if(n%i == 0 ){
-
-        long long start = i;
-        long long length = 0 ;
-        while(n%start == 0){
-            length++;
-            start++;
-        }
-        maxCount = max(maxCount ,length);
-
-        long long j = n/i;
-        start = j;
-        length = 0 ;
-        while(n%start == 0){
-            length++;
-            start++;
-        }
-        maxCount = max(maxCount ,length);  
-        } 
-    }
-    
-
-    cout << maxCount << endl;
-
+     int i =1;
+     while(n%i==0){
+        i++;
+     }
+    cout << i-1 << endl;
 }
 
 int main() {
