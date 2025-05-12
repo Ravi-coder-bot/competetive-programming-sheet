4f5828eeaf3b1beaa736a3d51cb84724623a658b
ravinder <ravibeniwal931@gmail.com>
2025-05-12 14:50:39 +0530

    upsolve 11 may contest

--- contests/11-may-div-2/question3.cpp
+++ contests/11-may-div-2/question3.cpp
@@ -1,67 +1,59 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> generateSpiralMatrix(int n) {
-    vector<vector<int>> matrix(n, vector<int>(n, 0));
-    if (n <= 0) return matrix;
+void solve(){
+    int n ;
+    cin >> n ;
+   vector<vector<int>> grid(n,vector<int>(n,0));
+   int sum = (n*n)-1;
+   int left = 0 ;
+   int right = n-1;
+   int top = 0;
+   int bottom = n-1;
 
-    int x, y;
-    if (n % 2 == 1) {
-        x = y = n / 2;
-    } else {
-        x = n / 2 - 1;
-        y = n / 2;
-    }
+   while(sum>=0 ){
+      for(int i =left ; i <= right && sum >=0 ;i++){
+         grid[top][i] = sum;
+         sum--;
+      }
+      top++;
 
-    int num = 0;
-    matrix[x][y] = num++;
-    
-    int directions[4][2] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-    int dir = 0;
-    int step = 1;
-    int steps_in_direction = 1;
-    
-    while (num < n * n) {
-        for (int i = 0; i < steps_in_direction && num < n * n; i++) {
-            x += directions[dir][0];
-            y += directions[dir][1];
-            if (x >= 0 && x < n && y >= 0 && y < n) {
-                matrix[x][y] = num++;
-            }
-        }
-        
-        dir = (dir + 1) % 4;
-        if (dir % 2 == 0) {
-            steps_in_direction++;
-        }
-    }
-    
-    return matrix;
-}
+      for(int i = top ; i<=bottom && sum >=0 ; i++){
+         grid[i][right] = sum;
+         sum--;
+      }
+      right--;
 
-void printMatrix(const vector<vector<int>>& matrix) {
-    int n = matrix.size();
-    for (const auto& row : matrix) {
-        for (int num : row) {
-            cout << num << " ";
-        }
-        cout << "\n";
-    }
+      for(int i =right; i >= left && sum >=0 ; i--){
+         grid[bottom][i] = sum;
+         sum--;
+      }
+      bottom--;
+
+      for(int i = bottom ; i >= top && sum >=0 ; i--){
+         grid[i][left] = sum;
+         sum--;
+      }
+      left++;
+   }
+
+
+   for(int i =0 ; i < n ; i++){
+      for(int j =0 ; j < n ; j++){
+         cout << grid[i][j] << " " ;
+      }
+      cout << endl;
+   }
+
+
+
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
     int t;
     cin >> t;
-    
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> matrix = generateSpiralMatrix(n);
-        printMatrix(matrix);
+    while(t--){
+        solve();
     }
     
     return 0;
