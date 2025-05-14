6b54053532a030a58b82fb43e32031322358c9ec
ravinder <ravibeniwal931@gmail.com>
2025-05-14 17:34:55 +0530

    code corrected 14th may

--- 900/11th.cpp
+++ 900/11th.cpp
@@ -1,34 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
-    int n ;
-    cin >> n ;
-
+void solve() {
+    int n;
+    cin >> n;
+    
     vector<int> arr(n);
-
-    for(int i =0 ; i < n ; i++){
+    for (int i = 0; i < n; ++i) {
         cin >> arr[i];
     }
-
-    for(int i =0 ; i < n-1 ; i++){
-        if(arr[i+1]%arr[i]==0){
-            arr[i]= arr[i+1];
-            arr[i+1] += 1;
+    
+    if (n == 1) {
+        cout << arr[0] << endl;
+        return;
+    }
+    
+    for (int i = 0; i < n - 1; ++i) {
+        if (arr[i+1] % arr[i] == 0) {
+            if (arr[i] == 1) {
+                arr[i]++;
+            } else {
+                arr[i]++;
+            }
         }
     }
-
-    for(auto i : arr){
-        cout << i << " ";
+    
+    for (int i = 0; i < n - 1; ++i) {
+        if (arr[i+1] % arr[i] == 0) {
+            arr[i+1]++;
+        }
     }
-   cout <<  endl;
-
+    
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int t;
-    cin >> t ;
-    while(t--){
+    cin >> t;
+    while (t--) {
         solve();
     }
     
