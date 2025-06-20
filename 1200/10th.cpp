fe3ff9346e66a26af4b101c476f728469a4eae55
ravinder <ravibeniwal931@gmail.com>
2025-06-20 14:43:35 +0530

    10th and 13th

---
+++ 1200/10th.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    long long n ;
+    long long q;
+    cin >> n >> q;
+
+    vector<long long> steps(n);
+    vector<long long> ques(q);
+
+    for (int i =0 ; i < n ; i++){
+        cin >> steps[i];
+    }
+
+    for(int i=0 ; i < q ; i++){
+        cin >> ques[i];
+    }
+
+    for(int i =0 ; i < q ; i++){
+        long long temp =0 ;
+        long long stop = ques[i];
+        for(int j =0 ; j < n ;j++){
+            if(steps[j] <= stop){
+                temp += steps[j];
+            }
+            else if(steps[j]>stop){
+                break;
+            }
+        }
+        cout << temp << ' ';
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long t;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
--- 1200/13th.cpp
+++ 1200/13th.cpp
@@ -2,41 +2,56 @@
 using namespace std;
 
 void solve(){
-     long long n , m;
-     cin >> n >>  m;
+     long long n, m;
+     cin >> n >> m;
 
      vector<long long> infected(m);
 
-     for(int i =0 ; i < m ; i++){
+     for(int i = 0; i < m; i++){
         cin >> infected[i];
      }
      sort(infected.begin(), infected.end());
      vector<long long> diff;
-
-     diff.push_back((infected[0]-1)+(n-infected[m-1]));
-     for(int i =0 ; i < m-1 ; i++){
-         diff.push_back(infected[i+1]-infected[i]-1);
+    
+     if((infected[0] - 1) + (n - infected[m-1]) > 0){
+         diff.push_back((infected[0] - 1) + (n - infected[m-1]));
+     }
+    
+     for(int i = 0; i < m - 1; i++){
+        if(infected[i+1] - infected[i] - 1 > 0){
+           diff.push_back(infected[i+1] - infected[i] - 1);  
+        } 
      }
 
-     sort(diff.begin(),diff.end(),greater<int>());
+     sort(diff.begin(), diff.end(), greater<long long>());
 
      int k = diff.size();
-     long long saved = 0 ;
+     long long saved = 0;
+     long long days = 0;  
 
-     for(int i = 0 ; i < k ; i++){
+     for(int i = 0; i < k; i++){
         long long gap = diff[i];
-        long long days_needed = 2*i;
-        saved += gap-(1+days_needed);
+        long long curr_gap = gap - days * 2;  
+        
+        if(curr_gap == 1 || curr_gap == 2){
+            saved++;
+            days++;
+            break;  
+        }
+        else if(curr_gap > 2){
+            saved += curr_gap - 1;  
+            days += 2; 
+        }
      }
 
-     cout << n-saved << "\n";
+     cout << n - saved << "\n";
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    long long t ;
+    long long t;
     cin >> t;
     while(t--){
         solve();
